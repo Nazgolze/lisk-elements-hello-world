27faee69b115ecc6f9d3a00f71c50390e679ff61
Juan Gonzalez <juan@lightcurve.io>
2018-07-04 09:13:45 +0200

    Remove my betanet account :)

--- test.js
+++ test.js
@@ -3,9 +3,10 @@
 var lisk = require('lisk-elements').default;
 
 var b = lisk.APIClient.createBetanetAPIClient();
-var your_passphrase = "YOUR PASSPHRASE HERE"
+var your_passphrase = "YOUR PASSPHRASE HERE";
+var your_lisk_id = "YOUR LISK ID";
 var transaction = lisk.transaction;
-var tx = lisk.transaction.transfer({ amount: "1", recipientId: "54834883818072291L", data: "Hello World", passphrase: your_passphrase });
+var tx = lisk.transaction.transfer({ amount: "1", recipientId: your_lisk_id, data: "Hello World", passphrase: your_passphrase });
 
 console.log(JSON.stringify(tx));
 
